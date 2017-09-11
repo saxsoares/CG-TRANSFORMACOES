5fd62c6e9e8260f5e1cb2e34107dc678a57166eb
Fernando Soares <sax.soares@gmail.com>
2017-09-11 00:14:34 -0300

    Adicionado declaração das cores aos headers

--- Transformacoes.h
+++ Transformacoes.h
@@ -2,8 +2,11 @@
 #define TRANSFORMACOES_H_INCLUDED
 #include <GL/glut.h>
 
-extern GLfloat  preto[], branco[], verde[], vermelho[], azul[], verde[], amarelo[],
-                cor7[], cor8[], cor9[], cor10[], cor11[], cor12[];
+extern GLfloat  preto[], branco[], verde[], vermelho[], azul[], amarelo[],
+        furchsia[], rosa[], rosaBri[], abobora[], ciano[],
+        verdeEsc[], azulEsc[], marinho[], fuligem[],
+        roxo[],azulCeu[],verdeGrama[],cinzaFosco[],
+        jambo[],laranja[],marrom[],cinza[], cinzaEsc[] ;
 GLint sw, sh, winPosX, winPosY;  // sw-> screen_width, sh->screen_height
 GLfloat w_width, w_height;
 
